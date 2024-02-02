a4c6fe4fa95f172098cda7d465a426bdcd6f80be
Fraser Spalding <2763283s@student.gla.ac.uk>
2024-02-02 15:02:26 +0000

    3;CHAPTER TESTS WORK

--- rango/views.py
+++ rango/views.py
@@ -6,9 +6,11 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     context_dict['categories']=category_list
-    return render(request, 'rango/index.html', context=context_dict)
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
 
 def about(request):
-    return render(request, "rango/about.html")
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
 
 
